cc5383e9a2b0bd23d3b02a0b1eda913cb2c8a6a7
Miriam Haile <miriam.haile@hotmail.com>
2019-01-21 15:59:19 +0100

    new classes

---
+++ src/Sticks.java
@@ -0,0 +1,15 @@
+public class Sticks {
+    private int sticks ;
+
+    public Sticks(int sticks) {
+        this.sticks = sticks;
+    }
+
+    public int getSticks() {
+        return sticks;
+    }
+
+    public void setSticks(int sticks) {
+        this.sticks = sticks;
+    }
+}
