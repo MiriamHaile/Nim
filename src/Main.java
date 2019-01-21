d4bf0b837229df1b360174e072c56b87dc2f1658
Linus Eriksson <linusaxel@gmail.com>
2019-01-21 16:06:28 +0100

    worked on main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
 
-    
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Sticks sticks = new Sticks(21);
+        int input;
+
+        while (!(sticks.getSticks() == 0)) {
+
+            System.out.print("Amount of sticks left: " + sticks.getSticks());
+            System.out.print("\nChoose how many sticks you want to take: ");
+            input = scanner.nextInt();
+
+            sticks.getSticks() - input;
+        }
+
+
+
+
+
+    }
 
 }
